afdbeffa4a870a7e33da94081b2662fc82e3f466
LUIS ANGEL <angel1.lasl46@gmail.com>
2026-05-04 11:18:19 -0500

    sd

--- APP-ONIEES/vite.config.js
+++ APP-ONIEES/vite.config.js
@@ -8,15 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: {   //ADD
-        host: '0.0.0.0',  // Permite conexiones externas
-        port: 5173,
-        strictPort: true,  // Fuerza usar este puerto
-        hmr: {     // ADD
-            host: '127.0.0.1',  // Tu IP local (NO localhost)
-            //host: '172.27.0.150',  // Tu IP local (NO localhost)
-            port: 5173,   //ADD
-        },
-        cors: true,  // Habilita CORS para otras PCs
-    },
 });
